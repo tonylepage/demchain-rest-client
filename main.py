cd770dfea71a6689f215df1c7d597e748e843870
tonylepage <lepage@outlook.com>
2022-04-20 18:06:21 +0800

    simplify split function

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def getAllMeasurements():
     print("utf8Result : %s", utf8Result)
     noLinesResult = utf8Result.replace('\r\n', '')
     print("noLinesResult : %s", noLinesResult)
-    pResult = json.loads(noLinesResult.split("status:200 payload:")[1][1:-4].replace('\\',''))
+    pResult = json.loads(noLinesResult.split("status:200 payload:")[1].replace('\\',''))
     fResult = json.dumps(pResult)
     print("final : %s", fResult)
     return {"Response": fResult}
