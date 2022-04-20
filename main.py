c38702864fe3183f2ad8dcd92485f0855cbf2bfd
tonylepage <lepage@outlook.com>
2022-04-20 19:40:50 +0800

    add CORS

--- main.py
+++ main.py
@@ -5,8 +5,21 @@ from subprocess import TimeoutExpired
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Measurement(BaseModel):
